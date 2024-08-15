eb0d89cca043456b5be01a16d28c5a0ec761ac10
Allen Wang <allencwang@google.com>
2024-08-15 20:35:25 +0000

    remove build folder, format

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
         # Add any dependencies here
     ],
     entry_points={
-        'console_scripts': [
-            'ray-tpu-setup=ray_tpu_setup.main:main',
+        "console_scripts": [
+            "ray-tpu-setup=ray_tpu_setup.main:main",
         ],
     },
     author="Allen Wang",
     author_email="allencwang@google.com",
     description="A tool to create Ray clusters on TPU pods",
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/allenwang28/ray-tpu-cluster",
     classifiers=[
@@ -23,5 +23,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.8",
 )
